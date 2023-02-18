23bb6b7dcfac96a5d519b8c1568917039a3505c0
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2023-02-19 00:14:57 +0530

    Update FrogJump_ninja.java

--- FrogJump_ninja.java
+++ FrogJump_ninja.java
@@ -1,3 +1,30 @@
+Problem Statement -->
+    There is a frog on the '1st' step of an 'N' stairs long staircase. 
+    The frog wants to reach the 'Nth' stair. 
+    'HEIGHT[i]' is the height of the '(i+1)th' stair.
+    If Frog jumps from 'ith' to 'jth' stair, the energy lost in the jump is given by absolute value of ( HEIGHT[i-1] - HEIGHT[j-1] ). 
+    If the Frog is on 'ith' staircase, he can jump either to '(i+1)th' stair or to '(i+2)th' stair.
+    Your task is to find the minimum total energy used by the frog to reach from '1st' stair to 'Nth' stair.
+   
+    If the given ‘HEIGHT’ array is [10,20,30,10], 
+    the answer 20 as the frog can jump from 1st stair to 2nd stair (|20-10| = 10 energy lost) and 
+    then a jump from 2nd stair to last stair (|10-20| = 10 energy lost).
+    So, the total energy lost is 20.
+        
+Detailed explanation ( Input/output format, Notes, Constraints, Images )
+Sample Input 1:
+2
+4
+10 20 30 10
+3
+10 50 10
+Sample Output 1:
+20
+0
+    
+    
+    
+    
 Solution 1--->
 import java.util.* ;
 import java.io.*; 
@@ -24,3 +51,46 @@ public class Solution {
         
         
     }
+
+    
+Solution 2-->
+        public static int frogJump(int n, int heights[]) {
+
+        // Write your code here..
+        int[] dp= new int[n];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        int ind;
+        int fs;
+        int ss = Integer.MAX_VALUE;
+        for(ind = 1; ind<n; ind++){
+             fs = dp[ind-1] + Math.abs(heights[ind]-heights[ind-1]);
+            if(ind > 1)
+                ss = dp[ind-2] + Math.abs(heights[ind]-heights[ind-2]);
+        
+        dp[ind] = Math.min(fs,ss);
+        }
+        return dp[n-1];
+    }
+
+
+
+Solution 3-->
+ public static int frogJump(int n, int heights[]) {
+    int prev=0;
+        int prev2=0;
+        int curr=0;
+        int ind;
+        int fs;
+        int ss = Integer.MAX_VALUE;
+        for(ind = 1; ind<n; ind++){
+             fs = prev + Math.abs(heights[ind]-heights[ind-1]);
+            if(ind > 1)
+                ss = prev2 + Math.abs(heights[ind]-heights[ind-2]);
+        
+        curr = Math.min(fs,ss);
+        prev2=prev;
+        prev=curr;
+        }
+        return prev;
+}
