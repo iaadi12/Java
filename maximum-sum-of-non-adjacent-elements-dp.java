1f0c40570199f9a1c692017f5560d61e6d9939bb
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2023-02-19 22:45:38 +0530

    Update maximum-sum-of-non-adjacent-elements-dp.java

--- maximum-sum-of-non-adjacent-elements-dp.java
+++ maximum-sum-of-non-adjacent-elements-dp.java
@@ -64,3 +64,32 @@ public class Solution {
 		return solveUtil(n-1, nums,dp);
 	}
 }
+
+
+Tabulation :-
+	Solution 1-->
+	import java.util.* ;
+import java.io.*; 
+import java.util.*;
+public class Solution {
+	static int solveUtil(int ind, ArrayList<Integer> arr,int[] dp){
+    if(ind<0)  return 0;
+    if(ind==0) return arr.get(ind);
+    if(dp[ind]!=-1) return dp[ind];
+    int pick= arr.get(ind)+ solveUtil(ind-2, arr,dp);
+    int nonPick = 0 + solveUtil(ind-1, arr,dp);
+    
+    return dp[ind]=Math.max(pick, nonPick);
+}
+	public static int maximumNonAdjacentSum(ArrayList<Integer> nums) {
+		// Write your code here.
+		int n=nums.size();
+		int[] dp = new int[n];
+		Arrays.fill(dp,-1);
+		return solveUtil(n-1, nums,dp);
+	}
+}
+
+
+Solution 2-->
+	
