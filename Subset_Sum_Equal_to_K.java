a3a01052f9b0794e76b032f8117d479c852579f2
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2023-03-03 17:07:05 +0530

    Update Subset_Sum_Equal_to_K.java

--- Subset_Sum_Equal_to_K.java
+++ Subset_Sum_Equal_to_K.java
@@ -30,23 +30,96 @@ public class Solution {
     if(ind == 0)
         return arr[0] == target;
     
-    if(dp[ind][target]!=-1)
-        return dp[ind][target]==0?false:true;
+    //if(dp[ind][target]!=-1)
+      //  return dp[ind][target]==0?false:true;
         
     boolean notTaken = fun(ind-1,target,arr,dp);
     
     boolean taken = false;
     if(arr[ind]<=target)
         taken = fun(ind-1,target-arr[ind],arr,dp);
-    dp[ind][target]=notTaken | taken?1:0;
+    //dp[ind][target]=notTaken | taken?1:0;
     return notTaken | taken;
 }
 
     public static boolean subsetSumToK(int n, int k, int arr[]){
         // Write your code here.
-        int dp[][]=new int[n][k+1];
-        for(int row[]: dp)
-            Arrays.fill(row,-1);
+        //int dp[][]=new int[n][k+1];
+        //for(int row[]: dp)
+           // Arrays.fill(row,-1);
         return fun(n-1,k, arr, dp);
+    }
+}
+
+
+TABULATION ---->>>>
+
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+
+    public static boolean subsetSumToK(int n, int k, int arr[]){
+        // Write your code here.
+         boolean dp[][]=new  boolean[n][k+1];
+        
+        for(int i=0; i<n; i++)
+            dp[i][0] = true;
+        
+       if(arr[0]<=k)
+        dp[0][arr[0]] = true;
+
+        for(int ind=1; ind<n; ind++){
+            for(int target=1; target<=k; target++){
+                 boolean notTaken = dp[ind-1][target];
+    
+                 boolean taken = false;
+                 if(arr[ind]<=target)
+                     taken = dp[ind-1][target-arr[ind]];
+
+                dp[ind][target]= notTaken || taken;     
+            }
+        }
+        return dp[n-1][k];
+
+    }
+}
+
+
+Space Optiization ---->>>
+
+
+import java.util.* ;
+import java.io.*; 
+public class Solution {
+
+
+    public static boolean subsetSumToK(int n, int k, int arr[]){
+        // Write your code here.
+        boolean [] prev = new boolean[k+1];
+        prev[0] = true;
+        
+        
+        
+       if(arr[0]<=k)
+        prev[arr[0]] = true;
+
+        for(int ind=1; ind<n; ind++){
+            boolean cur[]=new boolean[k+1];
+            cur[0] = true;
+            for(int target=1; target<=k; target++){
+                 boolean notTaken = prev[target];
+    
+                 boolean taken = false;
+                 if(arr[ind]<=target)
+                     taken = prev[target-arr[ind]];
+
+                cur[target]= notTaken || taken;     
+            }
+            prev = cur;
+        }
+        return prev[k];
+
     }
 }
