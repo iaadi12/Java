472b25dde1f81ed8549836adb11fd51cf5503d51
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2023-03-05 12:10:16 +0530

    Update House_Robber_I.java

--- House_Robber_I.java
+++ House_Robber_I.java
@@ -53,3 +53,49 @@ Constraints:
 
 TABULATION ---->>>
 
+class Solution {
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp,0);
+        dp[0] = nums[0];
+
+        for(int i=1; i<n; i++){
+            int take = nums[i];
+            if(i > 1){
+                take += dp[i-2];
+            }
+            int non_take = 0+dp[i-1];
+            dp[i] = Math.max(take, non_take);
+        }
+        return dp[n-1];
+    }
+}
+
+
+SPACEOPTIMIAZTION --->>>>
+
+class Solution {
+
+    public int rob(int[] nums) {
+        int n = nums.length;
+        int curr;
+        int prev = nums[0];
+        int prev2 = 0;
+
+        for(int i=1; i<n; i++){
+            int take = nums[i];
+            if(i > 1){
+                take += prev2;
+            }
+            int non_take = 0+prev;
+            curr = Math.max(take, non_take);
+            prev2=prev;
+            prev=curr;
+        }
+        
+        return prev;
+    }
+}
