95d5adb41f856a9dbfbb9a7ac2105ee2749c809c
Aaditya Kumar Sharma <97457902+iaadi12@users.noreply.github.com>
2023-03-05 11:50:01 +0530

    Create House_Robber_I.java

---
+++ House_Robber_I.java
@@ -0,0 +1,55 @@
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+ 
+
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 400
+  
+  
+  SOLUTION --->>>>
+  
+  Recursion & Memoization --->>>
+  
+  class Solution {
+
+    static int fun(int ind,int []nums, int[] dp)
+    {
+        if(ind == 0) return nums[ind];
+
+        if(ind < 0) return 0;
+        if(dp[ind] != -1) return dp[ind];
+        int pick = nums[ind] + fun(ind-2,nums,dp);
+        int not_pick = 0 + fun(ind-1,nums,dp);
+
+        return dp[ind] = Math.max(pick, not_pick);
+    }
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return fun(n-1,nums,dp);
+    }
+}
+
+
+TABULATION ---->>>
+
